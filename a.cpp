cca758699081f4f92ef043af5c7a6bb39e8e0a24
ABH1516 <abhinavkaushal78@gmail.com>
2021-02-09 16:41:18 +0530

    first commit to the repo

--- a.cpp
+++ a.cpp
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
+
 #include<iostream>
 using namespace std;
 
 int main(){
-    cout<<" HELLO GITHUB";
+    cout<<" HELLO Github";
+    cout<<" HAIL TBC"
     return 0;
-=======
-#include <iostream>
-
-using namespace std;
-
-main(){
-    cout<<"Hello github";
->>>>>>> 473fdfc50237a861b9e3ac0011441c3555b3df47
+    
 }
