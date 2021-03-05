1c8055f8c8b890e80dc6e7e98ff9f658bb280250
chinmaypant21 <pant.chinu.2@gmail.com>
2021-03-06 02:56:25 +0530

    Array size bug fixed temporarily

--- Dual Converter/dual.cpp
+++ Dual Converter/dual.cpp
@@ -5,17 +5,18 @@ using namespace std;
 class LPP
 {
     public:
-        int num_of_variable {};
-        int num_of_constaint {};
+        static const int num_of_variable = {2};
+        static const int num_of_constraint {2};
 };
 
 class Objective :public LPP
 {
-    int cost_Coeff[num_of_constaint] {};
+    int cost_Coeff[num_of_constraint] {};
 
+    public:
     void setCostCoeff()
     {
-        for (int i = 0; i < num_of_variable; i++)
+        for (int i = 1; i <= num_of_variable; ++i)
         {
             cout<<"\nX"<<i<<" : ";
             cin>>cost_Coeff[i];
@@ -24,16 +25,18 @@ class Objective :public LPP
 };
 
 class Constraint :public LPP
-{
-    int coeff_matrix[num_of_constaint][num_of_variable] {};
-
-    for (int i = 0; i < num_of_constraint; i++)
+{   
+    int coeff_matrix[num_of_constraint][num_of_variable];
+    void setCoeffMatrix()
     {
-        cout<<"\nFor constraint "<<i<<endl;
-        for(int j = 0; j <num_of_variable; j++)
+        for(int i = 0; i < num_of_constraint; i++)
         {
-            cout<<"\nX"<<j<<" : ";
-            cin>>coeff_matrix[i][j];
+            cout<<"\nFor constraint "<<i<<endl;
+            for(int j = 0; j <num_of_variable; j++)
+            {
+                cout<<"\nX"<<j<<" : ";
+                cin>>coeff_matrix[i][j];
+            }
         }
     }
 
@@ -41,5 +44,9 @@ class Constraint :public LPP
 
 int main()
 {
+    LPP l;
+    // l.setNumVar();
+    Objective o;
+    o.setCostCoeff();
     return 0;
 }
---
+++ Dual Converter/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+nt v;
+        // cout<<"\nEnter Number of variables";
+        // cin>>v;
+        // return v;
